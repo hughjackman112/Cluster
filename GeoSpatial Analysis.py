976bcb33522b5661c424c82e647ad8a39a478a1f
hughjackman112 <84380754+hughjackman112@users.noreply.github.com>
2023-07-25 18:14:00 -0400

    Add files via upload

---
+++ GeoSpatial Analysis.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point
+
+# Define filepath
+filepath = r"C:\Users\PrashanthKotha\OneDrive - Millennial Specialty Insurance LLC\Documents\Geospatial Analysis\ESJune23\WildFire\NewWildfire.xlsx"
+
+# Load Excel file using Pandas
+f = pd.read_excel(filepath, sheet_name=None)
+
+# Iterate through each worksheet
+for name, df in f.items():
+    
+
+    df.columns = df.columns.str.strip()  # Strip whitespace from column names
+    print(df.columns)  # Print column names to verify
+
+    # Step 2: Create Point geometries from latitude and longitude
+    geometry = [Point(xy) for xy in zip(df['Longitude'], df['Latitude'])]
+
+    # Step 3: Convert DataFrame to GeoDataFrame
+    gdf = gpd.GeoDataFrame(df, crs='EPSG:4326', geometry=geometry)
+    
+    # Step 4: Create rings of 5-mile radius around each point
+    gdf['buffered_geometry'] = gdf.geometry.buffer(5 / 69)  # Assuming 1 degree is approximately 69 miles
+    
+    # Step 5: Count points within each ring
+    gdf['points_within_ring'] = gdf.apply(
+        lambda row: gdf[gdf.geometry.within(row['buffered_geometry'])].shape[0], axis=1)
+    
+    # Step 6: Sort and find the top 20 clusters
+    top_20_clusters = gdf.sort_values(by='points_within_ring', ascending=False).head(10)
+    
+    # Step 7: Save the top 20 clusters as the first Excel file
+    output_file_path_clusters = r"C:\Users\PrashanthKotha\OneDrive - Millennial Specialty Insurance LLC\Documents\Geospatial Analysis\ESJune23\WildFire\{}.xlsx".format(name)
+    top_20_clusters.to_excel(output_file_path_clusters, index=False)
+    print("Top 20 clusters saved to Excel file:", output_file_path_clusters)
+    
+    # Step 8: Create a new GeoDataFrame with the center points of the rings
+    center_points = gdf.copy()
+    center_points.geometry = center_points['buffered_geometry'].centroid
+    
+    # Step 9: Save the center points as the second Excel file
+    output_file_path_center_points = r"C:\Users\PrashanthKotha\OneDrive - Millennial Specialty Insurance LLC\Documents\Geospatial Analysis\ESJune23\WildFire\{}.xlsx".format(name)
+    center_points.to_excel(output_file_path_center_points, index=False)
+    print("Center points of rings saved to Excel file:", output_file_path_center_points)
---
+++ The_OG_Code.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  3 12:37:05 2023
+
+@author: PrashanthKotha
+"""
+
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+os.environ['PATH'] += r"C:/SeleniumDriver"
+driver = webdriver.Chrome()
+driver.get("https://dev-admin-msimga.azurewebsites.net/")
+my_email = driver.find_element(by='xpath', value='//*[@id="LoginName"]')
+my_email.send_keys('ProductTS@msimga.com')
+my_pw = driver.find_element(by='xpath', value='//*[@id="LoginPassword"]')
+my_pw.send_keys('Product1')
+my_signin = driver.find_element(by='xpath', value='//*[@id="buttonSignIn"]')
+my_signin.click()
+
+products_button = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Products')]"))
+)
+driver.execute_script("arguments[0].click();", products_button)
+
+workbooks_option = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Workbooks')]"))
+)
+driver.execute_script("arguments[0].click();", workbooks_option)
+
+file_input = driver.find_element(By.ID, 'productworkbookfile')
+upload_button = driver.find_element(By.ID, 'buttonUploadWorkbook')
+
+folder_path = 'C:\\Users\\PrashanthKotha\\OneDrive - Millennial Specialty Insurance LLC\\Documents\\1_Dev'
+file_list = os.listdir(folder_path)
+uploaded_files = []
+
+for file in file_list:
+    dropdown_element = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "UploadWorkbook_UwCarrierPartyID"))
+    )
+
+    dropdown = Select(dropdown_element)
+    dropdown.select_by_visible_text("Transverse Specialty Insurance Company")
+
+    file_path = os.path.join(folder_path, file)
+    file_input.send_keys(file_path)
+    upload_button.click()
+
+    
+    timeout = 60
+    start_time = time.time()
+
+    while time.time() - start_time < timeout:
+        try:
+            success_message = driver.find_element(By.ID, "uploadworkbook-response")
+            if success_message.text == "Workbook successfully uploaded":
+                uploaded_files.append((file, "Yes"))
+                print(f"File '{file}' successfully uploaded.")
+                break  
+        except:
+            pass
+
+        time.sleep(1)
+
+    if time.time() - start_time >= timeout:
+        uploaded_files.append((file, "No"))
+        print(f"File '{file}' upload failed.")
+
+driver.quit()
+
+print("List of uploaded files:")
+for file, status in uploaded_files:
+    print(f"{file}: {status}")
